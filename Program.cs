291d39aa87b083cb6dd9e40738cc1062d512f8c3
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-09-01 19:47:14 +0530

    FIRST COMMIT

--- Program.cs
+++ Program.cs
@@ -2,11 +2,30 @@
 
 namespace DestructorDemo
 {
+    class Employee
+    {
+        int empID = 1001;
+        string empname = "Vivek Jadhav";
+        public Employee()
+        {
+            Console.WriteLine("Contructor of Employee class");
+        }
+        ~Employee()
+        {
+            Console.WriteLine("Destroy object of Employee");
+        }
+        public void printDetails()
+        {
+            Console.WriteLine("Employee ID: {0} || Employee Name: {1}", empID, empname);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Employee emp1 = new Employee();
+            emp1.printDetails();
+            // Employee EMP2 = new Employee();
         }
     }
 }
